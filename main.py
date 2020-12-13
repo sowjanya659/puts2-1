9bd8f06f9a81501d3caeae38190a993b4bb3142f
sowjanya659 <alla.sowjanya659@gmail.com>
2020-12-13 19:41:50 +0100

    main code added

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from flask import Flask, request
 from fractions import Fraction
-import json
-import statistics
+import json, statistics
 import decimal
 from decimal import Decimal
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-
     if request.method == 'GET':
         value1 = request.args.get('X', default=0, type=str)
 
@@ -22,21 +19,20 @@ def index():
         return error
 
     return value1
-
-
-@app.route('/max')#as per requirement
-def maximum():
+@app.route('/median')#only one function as per requirement
+def median():
     try:
         value1 = index()
-        result = max(value1)#finds maximum value
+        result = statistics.median(value1)#use statistics lib
     except ValueError:
         error = index()
         return error
     else:
         if float(result).is_integer():
-            answer = int(result)
-            return "%d" % result
-        return str(float(round(result)))
+            result = int(result)
+            return result
+        return str(float(result))
+
 
 if __name__ == "__main__":
     app.run()
