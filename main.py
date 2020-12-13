8b750f87c5f511acf467dff0e6db999b97eb4af5
sowjanya659 <alla.sowjanya659@gmail.com>
2020-12-13 19:49:58 +0100

    min main code is added

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 from flask import Flask, request
+from fractions import Fraction
+import json
+import statistics
+import decimal
+from decimal import Decimal
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
+
+    if request.method == 'GET':
+        value1 = request.args.get('X', default=0, type=str)
+
+    else:
+        value1 = request.values.get('X', default=0, type=str)
+    try:
+        value1 = [Fraction(value) for value in value1.split(',')]
+    except ValueError:
+        error = "please enter proper input"
+        return error
+
+    return value1
 
 
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
-
+@app.route('/min')#as per requiremet
+def minimum():
+    try:
+        value1 = index()
+        result = min(value1)#takes minimum value
+    except ValueError:
+        error = index()
+        return error
+    else:
+        if float(result).is_integer():
+            answer = int(result)
+            return "%d" % result
+        return str(float(round(result)))#displays output in the form of 1.0
 
 if __name__ == "__main__":
     app.run()
+
