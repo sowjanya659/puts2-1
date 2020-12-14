27a3d12c1952a707fd31e0ad5450c7a6a6c8d32c
sowjanya659 <alla.sowjanya659@gmail.com>
2020-12-14 16:50:43 +0100

    main code is added

--- main.py
+++ main.py
@@ -1,19 +1,84 @@
 from flask import Flask, request
-
+from fractions import Fraction
+import json, statistics
+import decimal
+from decimal import Decimal
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
+    if request.method == 'GET':
+        value1 = request.args.get('X', default=0, type=str)
 
+    else:
+        value1 = request.values.get('X', default=0, type=str)
+    try:
+        value1 = [Fraction(value) for value in value1.split(',')]
+    except ValueError:
+        error = "Please enter proper input"
+        return error
 
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
+    return value1
+@app.route('/average')#to support all three functions
+@app.route('/mean')
+@app.route('/avg')
+def mean():
+    try:
+        value1 = index()
+        result = statistics.mean(value1)
+    except ValueError:
+        error = index()
+        return error
+    else:
+        if float(result).is_integer():
+            result = int(result)
+            return result
+        return str(float(result))#display the solution in float i.e. in the form of 1.0
+
+@app.route('/median')#only one function as per requirement
+def median():
+    try:
+        value1 = index()
+        result = statistics.median(value1)#use statistics lib
+    except ValueError:
+        error = index()
+        return error
+    else:
+        if float(result).is_integer():
+            result = int(result)
+            return result
+        return str(float(result))
+
+@app.route('/max')#as per requirement
+def maximum():
+    try:
+        value1 = index()
+        result = max(value1)#finds maximum value
+    except ValueError:
+        error = index()
+        return error
+    else:
+        if float(result).is_integer():
+            answer = int(result)
+            return "%d" % result
+        return str(float(round(result)))
+
+@app.route('/min')#as per requiremet
+def minimum():
+    try:
+        value1 = index()
+        result = min(value1)#takes minimum value
+    except ValueError:
+        error = index()
+        return error
+    else:
+        if float(result).is_integer():
+            answer = int(result)
+            return "%d" % result
+        return str(float(round(result)))#displays output in the form of 1.0
+
 
 
 if __name__ == "__main__":
     app.run()
+
